69c4a4b3a7085f8d5afdeafceebec3cccb20bc4b
Lucy Lytle <lucy.lytle@kellogg.northwestern.edu>
2024-04-25 14:28:43 +0000

    ruby hw

--- practice.rb
+++ practice.rb
@@ -27,3 +27,61 @@ blockchain = [
 # Anthony's KelloggCoin balance is 2650
 
 # 👇👇👇 Your code HERE 👇👇👇
+
+from_ben = 0 
+to_ben = 0 
+from_brian = 0 
+to_brian = 0 
+from_evan = 0 
+to_evan = 0 
+from_anthony = 0 
+to_anthony = 0 
+
+blockchain.each do |transaction|
+#Ben's wallet
+if transaction["from_user"] == "ben"
+  from_ben = from_ben + transaction["amount"]
+end 
+
+if transaction["to_user"] == "ben"
+  to_ben = to_ben + transaction["amount"]
+end 
+
+#Brian's wallet
+if transaction["from_user"] == "brian"
+  from_brian = from_brian + transaction["amount"]
+end 
+
+if transaction["to_user"] == "brian"
+  to_brian = to_brian + transaction["amount"]
+end 
+
+#Evan's wallet
+if transaction["from_user"] == "evan"
+  from_evan = from_evan + transaction["amount"]
+end 
+
+if transaction["to_user"] == "evan"
+  to_evan = to_evan + transaction["amount"]
+end 
+
+#Anthony's wallet
+if transaction["from_user"] == "anthony"
+  from_anthony = from_anthony + transaction["amount"]
+end 
+
+if transaction["to_user"] == "anthony"
+  to_anthony = to_anthony + transaction["amount"]
+end 
+
+end 
+
+  ben_wallet = -from_ben + to_ben
+  brian_wallet = -from_brian + to_brian
+  evan_wallet = -from_evan + to_evan
+  anthony_wallet = -from_anthony + to_anthony
+
+p "Ben's KelloggCoin balance is #{ben_wallet}"
+p "Brian's KelloggCoin balance is #{brian_wallet}"
+p "Evan's KelloggCoin balance is #{evan_wallet}"
+p "Anthony's KelloggCoin balance is #{anthony_wallet}"
